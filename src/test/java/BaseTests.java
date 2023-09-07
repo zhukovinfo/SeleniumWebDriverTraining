113b9f5684ddd4662ccda0c93f7c2fbc3d24746a
Andrey Zhukov <zhukovinfo@gmail.com>
2023-09-07 10:01:14 +0400

    added login scenario

--- src/test/java/BaseTests.java
+++ src/test/java/BaseTests.java
@@ -1,14 +1,28 @@
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
 public class BaseTests {
+  WebDriver webDriver;
+
+  @BeforeClass
+  public void setUp() {
+    webDriver = new ChromeDriver();
+    webDriver.get(" http://localhost/litecart/admin/");
+  }
 
   @Test
   public void test() {
-    WebDriver webDriver = new ChromeDriver();
-    webDriver.get("http://www.yandex.ru");
-    webDriver.quit();
+    webDriver.findElement(By.name("username")).sendKeys("admin");
+    webDriver.findElement(By.name("password")).sendKeys("admin");
+    webDriver.findElement(By.name("login")).click();
   }
 
+  @AfterClass(alwaysRun = true)
+  public void tearDown() {
+    webDriver.quit();
+  }
 }

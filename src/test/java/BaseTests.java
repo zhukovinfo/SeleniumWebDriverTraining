cf96485842b8986356a036689aea55b61ffaf69c
Andrey Zhukov <zhukovinfo@gmail.com>
2023-09-13 18:53:01 +0400

    added 'Opening main menu items' scenario

--- src/test/java/BaseTests.java
+++ src/test/java/BaseTests.java
@@ -1,28 +1,51 @@
+import static org.assertj.core.api.Java6Assertions.assertThat;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.testng.annotations.AfterClass;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
+import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 public class BaseTests {
+
   WebDriver webDriver;
 
-  @BeforeClass
+  @BeforeSuite
   public void setUp() {
-    webDriver = new ChromeDriver();
-    webDriver.get(" http://localhost/litecart/admin/");
+    FirefoxOptions options = new FirefoxOptions();
+    options.setBinary("C:\\Program Files\\Firefox Nightly\\firefox.exe");
+    webDriver = new FirefoxDriver(options);
   }
 
-  @Test
-  public void test() {
+  @BeforeClass
+  public void login() {
+    webDriver.get(" http://localhost/litecart/admin/");
     webDriver.findElement(By.name("username")).sendKeys("admin");
     webDriver.findElement(By.name("password")).sendKeys("admin");
     webDriver.findElement(By.name("login")).click();
   }
 
-  @AfterClass(alwaysRun = true)
+  @Test(dataProvider = "menuItemsData")
+  public void test(String menuItemText) {
+    webDriver.findElement(By.linkText(menuItemText)).click();
+    assertThat(webDriver.findElements(By.tagName("h1")).size())
+        .describedAs("Проверка наличия заголовка h1 на странице")
+        .isEqualTo(1);
+  }
+
+  @DataProvider(name = "menuItemsData")
+  public static Object[] mainMenuItemsTexts() {
+    return new Object[]{"Appearence", "Catalog", "Countries", "Currencies", "Customers", "Geo Zones", "Languages",
+        "Modules", "Orders", "Pages", "Reports", "Settings", "Slides", "Tax", "Translations", "Users", "vQmods"};
+  }
+
+  @AfterSuite(alwaysRun = true)
   public void tearDown() {
+    webDriver.get("http://localhost/litecart/admin/logout.php");
     webDriver.quit();
   }
 }

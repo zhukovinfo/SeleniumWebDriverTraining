4c39f664d0b0e522a7bce58d3494ff5f0fd6b040
Andrey Zhukov <zhukovinfo@gmail.com>
2023-09-26 17:49:00 +0400

    added test to open new windows

---
+++ src/test/java/WindowsTests.java
@@ -0,0 +1,29 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.testng.annotations.Test;
+
+public class WindowsTests extends BaseTest {
+
+  @Test
+  public void openWindowsTest() {
+    webDriver.get("http://localhost/litecart/admin/?app=countries&doc=countries");
+    webDriver.findElement(By.linkText("Add New Country")).click();
+    String originalWindow = webDriver.getWindowHandle();
+    int windowCount = webDriver.getWindowHandles().size();
+    webDriver.findElements(By.xpath("//i[@class='fa fa-external-link']"))
+        .forEach(link -> {
+          link.click();
+          wait.until(ExpectedConditions.numberOfWindowsToBe(windowCount + 1));
+
+          webDriver.getWindowHandles().forEach(windowHandle -> {
+            if (!windowHandle.equals(originalWindow)) {
+              webDriver.switchTo().window(windowHandle);
+              wait.until(ExpectedConditions.not(ExpectedConditions.titleIs("")));
+              System.out.println("Открыта новая страница: " + webDriver.getTitle());
+              webDriver.close();
+              webDriver.switchTo().window(originalWindow);
+            }
+          });
+        });
+  }
+}

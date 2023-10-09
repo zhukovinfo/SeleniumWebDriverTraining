a4ba02125b5bcf5c642b4463ee20efd5e4d1f319
Andrey Zhukov <zhukovinfo@gmail.com>
2023-10-09 11:17:42 +0400

    added taking screenshot method

--- src/test/java/com/zhukov/training/BaseTest.java
+++ src/test/java/com/zhukov/training/BaseTest.java
@@ -1,11 +1,18 @@
 package com.zhukov.training;
 
+import java.io.File;
+import java.io.IOException;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.nio.file.Files;
 import java.time.Duration;
 import java.util.List;
 import java.util.NoSuchElementException;
 import java.util.logging.Level;
 import java.util.stream.IntStream;
 import org.openqa.selenium.By;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -14,6 +21,8 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.firefox.FirefoxOptions;
 import org.openqa.selenium.logging.LoggingPreferences;
 import org.openqa.selenium.remote.Browser;
+import org.openqa.selenium.support.events.EventFiringDecorator;
+import org.openqa.selenium.support.events.WebDriverListener;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
@@ -22,6 +31,28 @@ public class BaseTest {
 
   protected WebDriver webDriver;
   protected WebDriverWait wait;
+
+  public static class MyListener implements WebDriverListener {
+
+    @Override
+    public void onError(Object target, Method method, Object[] args, InvocationTargetException e) {
+      WebDriverListener.super.onError(target, method, args, e);
+      System.out.println("Ошибка " + e.getTargetException().toString());
+      takeScreenshot((WebDriver) target);
+    }
+
+    private void takeScreenshot(WebDriver driver) {
+      File tempFile = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
+      File file = new File("screen" + System.currentTimeMillis() + ".png");
+      try {
+        Files.copy(tempFile.toPath(), file.toPath());
+      } catch (IOException e) {
+        e.printStackTrace();
+      }
+      System.out.println(file);
+    }
+  }
+
 
   @BeforeClass
   public void setUp() {
@@ -42,14 +73,15 @@ public class BaseTest {
       loggingPreferences.enable("browser", Level.ALL);
       ChromeOptions chromeOptions = new ChromeOptions();
       chromeOptions.setCapability("goog:loggingPrefs", loggingPreferences);
-      return new ChromeDriver(chromeOptions);
+      return new EventFiringDecorator<>(new MyListener()).decorate(new ChromeDriver(chromeOptions));
     }
     if (Browser.FIREFOX.browserName().equals(name)) {
       FirefoxOptions options = new FirefoxOptions();
       options.setBinary("C:\\Program Files\\Firefox Nightly\\firefox.exe");
-      return new FirefoxDriver(options);
-    } else
+      return new EventFiringDecorator<>(new MyListener()).decorate(new FirefoxDriver(options));
+    } else {
       throw new IllegalArgumentException("Unknown browser name specified");
+    }
   }
 
   private void login() {
@@ -58,6 +90,7 @@ public class BaseTest {
     webDriver.findElement(By.name("password")).sendKeys("admin");
     webDriver.findElement(By.name("login")).click();
   }
+
 
   public int getColumnIdByName(WebElement table, String columnName) {
     List<WebElement> headers = table.findElement(By.className("header")).findElements(By.tagName("th"));
@@ -68,15 +101,15 @@ public class BaseTest {
         .orElseThrow(() -> new NoSuchElementException("Column '" + columnName + "' not found in the table"));
   }
 
-  boolean isElementPresented(By locator) {
+  public boolean isElementPresented(By locator) {
     try {
       webDriver.findElement(locator);
       return true;
-    }
-    catch (org.openqa.selenium.NoSuchElementException ex) {
+    } catch (org.openqa.selenium.NoSuchElementException ex) {
       return false;
     }
   }
+
   @AfterClass(alwaysRun = true)
   public void tearDown() {
     webDriver.get("http://localhost/litecart/admin/logout.php");
